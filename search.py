e00e856d563fd78a2e830f78a74a8fe6cdb89e3b
Ryan Reuther <53404830+rreuther8@users.noreply.github.com>
2023-07-10 15:31:34 -0400

    Update search.py

--- search.py
+++ search.py
@@ -1,28 +1,22 @@
 #remove unnecessary imports
 import os
 import re
-import logging
 import click
 
 import pandas as pd
 
 
-
 SEPARATORS = [',', '|', '\t']
-FILE_SEARCH_LOG = 'Seaching for files containing "{}".'
-NO_FILE_EXCEPTION = 'No files names containing "{}" keyword.'
-NO_COLUMN_EXCEPTION = 'No columns names containing "{}" keyword.'
-COLUMN_MATCH_LOG = 'Column Regex Match for "{}":'
-NO_SEPARATOR_EXCEPTION = 'No separator found in file.'
+SEARCH_LOG = 'Searching for {} containing "{}".'
+SEARCH_FINISH_LOG = 'Completed search.'
+SEARCH_FAILURE_LOG = 'SEARCH FAILED.'
 
-#instantiate the logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+SEARCH_FAILURE_EXCEPTION = 'No {} containing "{}" keyword.'
+MATCH_LOG = 'Located {} regex match for "{}":'
+
 
 #create a function that finds all file names that match the regex
 def get_filename_from_regex(file_regex):
-	
-
 	#get all files in the current directory
 	files = os.listdir()
 
@@ -32,7 +26,7 @@ def get_filename_from_regex(file_regex):
 
 	#raise exception if no files are found
 	if not regex_files:
-		raise ValueError(NO_FILE_EXCEPTION.format(file_regex))
+		raise ValueError(SEARCH_FAILURE_EXCEPTION.format('files', file_regex))
 	return regex_files
 
 
@@ -44,16 +38,18 @@ def infer_separator(filename):
 			if separator in first_line:
 				return separator
 		else:
-			raise ValueError(NO_SEPARATOR_EXCEPTION)
+			raise ValueError(SEARCH_FAILURE_EXCEPTION.format('separators', file_regex))
 
 
 def read_files(filenames):
 	separator = infer_separator(filenames[0])
 
 	#read all files in a pandas dataframe and include the filename as a column
-	df = pd.concat([pd.read_csv(filename, sep=separator).assign(filename=filename) for filename in filenames])
+	dataframe = pd.concat([pd.read_csv(filename, sep=separator).assign(filename=filename) for filename in filenames])
 
-	return df
+	if len(dataframe) == 0:
+		raise ValueError(SEARCH_FAILURE_EXCEPTION.format('files', column_regex))
+	return dataframe
 
 
 def get_column_regex_matches(df, column_regex):
@@ -66,45 +62,60 @@ def get_column_regex_matches(df, column_regex):
 	return columns
 
 
-
-
 def get_column_group_counts(dataframe, columns):
 	# in pandas get the distinct counts for each value in a column
 	groupings_dict = { column: dataframe[column].value_counts().reset_index(name='counts') for column in columns }
 	
 	#raise exception if groupings_dict is empty
 	if not groupings_dict:
-		raise ValueError(NO_COLUMN_EXCEPTION.format(column_regex))
+		raise ValueError(SEARCH_FAILURE_EXCEPTION.format('column', column_regex))
 
 	return groupings_dict
 
 
 #create a function to take a pandas dataframe, and filter it to ensure that there are no null values in a list of columns.
 def filter_null_values(dataframe, columns):
-	
-	for column in columns:
-		filtered_dataframe = df[column].notnull()
-	return filters
+	filtered_dataframe = dataframe.copy()
 
+	for column in columns:
+		filtered_dataframe = filtered_dataframe[filtered_dataframe[column].notnull()]
+
+	return filtered_dataframe
+
+
+#write a function filter_for_regex_pattern to take a pandas dataframe, 
+#a list of columns, and a regex pattern, and return a dataframe filtered to only include rows where the value in the column matches the regex pattern.
+def filter_for_regex_pattern(filtered_df, columns, value_regex):
+	value_df = filtered_df.copy()
+
+	for column in columns:
+		value_df = value_df[value_df[column].str.contains(value_regex, na=False)]
+
+	return value_df
 
 
 def get_column_value_regex_matches(dataframe, columns, value_regex):
-	df_filtered = filter_null_values(dataframe, columns)
-	df_filtered = df.where(F.expr(filters)).persist()
+	filtered_df = filter_null_values(dataframe, columns)
 
-	column_dict = {column: df_filtered.filter(F.col(column).contains(value_regex)).count() for column in columns}
-	
-	return column_dict
+	value_df = filter_for_regex_pattern(filtered_df, columns, value_regex)
+
+	value_dict = get_column_group_counts(value_df, columns)
+
+	#raise exception if value_dict is empty
+	if value_dict == 0:
+		raise ValueError(NO_COLUMN_EXCEPTION.format(column_regex))
+
+	return value_dict
 
 
-
-
-def log(columns, column_regex, column_groupings):
-	logger.info(
-		'\n'.join([
-		COLUMN_MATCH_LOG.format(column_regex),
-		'\n\t'.join(columns)
-	]))
+def log(regex, groupings, level):
+	for key, dataframe in groupings.items():
+		print(
+			'\n\n'.join([
+			MATCH_LOG.format(level, regex),
+			dataframe.to_string(),
+			''
+		]))
 
 
 @click.command()
@@ -113,26 +124,28 @@ def log(columns, column_regex, column_groupings):
 @click.option("--value_regex", "-v", default=None)
 def main(file_regex, column_regex, value_regex):
 	try:
-		
-		
-		#log an error message saying "hello"
-		logger.info('Hello')
 
+		print(SEARCH_LOG.format('files', file_regex))
 		regex_filenames = get_filename_from_regex(file_regex)
-		df = read_files(regex_filenames)
+		dataframe = read_files(regex_filenames)
 
-		columns = get_column_regex_matches(df, column_regex)
-		column_groupings = get_column_group_counts(df, columns)
-		log(columns, column_regex, column_groupings)
+		filename_dict = get_column_group_counts(dataframe, ['filename'])
+		log(file_regex, filename_dict, 'file')
 
-		import ipdb; ipdb.set_trace()
+		print(SEARCH_LOG.format('columns', column_regex))
+		columns = get_column_regex_matches(dataframe, column_regex)
+		column_dict = get_column_group_counts(dataframe, columns)
+		log(column_regex, column_dict, 'column')
 
 		if value_regex:
-			column_dict = get_column_value_regex_matches(df, columns, value_regex)
-			# LOG
-	
+			print(SEARCH_LOG.format('column values', value_regex))
+			value_dict = get_column_value_regex_matches(dataframe, columns, value_regex)
+			log(value_regex, value_dict, 'column value')
+
+	except:
+		print(SEARCH_FAILURE_LOG)
 	finally:
-		logger.error("Goodbye")
+		print(SEARCH_FINISH_LOG)
 
 
 if __name__ == "__main__":
